e88b06f84ad92f670911964958297734d1c9415c
ganeshpapani <ganeshpapani9550@gmail.com>
2023-05-18 11:18:45 +0000

    solution

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import UserProfile from './components/UserProfile/index'
+
+const userDetails = {
+  imageUrl: 'https://assets.ccbp.in/frontend/react-js/esther-howard-img.png',
+  name: 'patel',
+  role: 'data analyst',
+}
+
+const App = () => <UserProfile />
+
+export default App
